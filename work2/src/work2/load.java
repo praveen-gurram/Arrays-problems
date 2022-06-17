6f054dd895c4d1d069add37a2c0ebf44de282efa
praveen <praveen@praveen>
2022-06-17 07:13:33 +0530

    Arrays

---
+++ work2/src/work2/load.java
@@ -0,0 +1,30 @@
+package work2;
+
+  // Arrays lo biggest element and smallest element;
+ /*public class load {
+
+    public static void main(String[] args) {
+
+       int arr[]= {10,20,50,80,90,40,30};
+       int small=arr[0];
+       int big =arr[0];
+       for(int i=0;i<arr.length;++i)
+       {
+    	   if(big<arr[i])
+    	   {
+    		   big=arr[i];
+    	   }
+    	   if(small>arr[i])
+    	   {
+    	     small=arr[i];
+    	   }
+       }
+       System.out.println(" big :"+big);
+       System.out.println(" small :"+small);
+       
+    }
+
+
+	}*/
+
+
